2d6680babcacf1bb03b0be009f03fa02a8ac52d4
Megan Ekse <meganekse@gmail.com>
2021-10-11 01:02:33 -0700

    created file structure and server basics

---
+++ models/entry.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Entry extends Model {}
+
+Entry.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    entry_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    entry_text: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'entry',
+  }
+);
+
+module.exports = Entry;
